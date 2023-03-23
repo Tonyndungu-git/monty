5748f13480128177a082ba333c4fff45ef825f57
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-03-23 15:03:03 +0300

    added a rotr instruction

--- rotl.c
+++ rotl.c
@@ -7,21 +7,21 @@
  */
 void rotl(stack_t **stack, unsigned int line_number)
 {
-    stack_t *last_node, *second_last_node;
+	stack_t *last_node, *second_last_node;
 
-    (void) line_number; /* unused parameter */
+	(void) line_number; /* unused parameter */
 
-    if (*stack == NULL || (*stack)->next == NULL)
-        return;
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
 
-    last_node = *stack;
-    while (last_node->next != NULL)
-        last_node = last_node->next;
-    second_last_node = last_node->prev;
+	last_node = *stack;
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+	second_last_node = last_node->prev;
 
-    last_node->next = *stack;
-    (*stack)->prev = last_node;
-    *stack = last_node;
-    second_last_node->next = NULL;
-    last_node->prev = NULL;
+	last_node->next = *stack;
+	(*stack)->prev = last_node;
+	*stack = last_node;
+	second_last_node->next = NULL;
+	last_node->prev = NULL;
 }
