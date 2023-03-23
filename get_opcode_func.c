e460b4f7e1d44928704bd23c8afc74009a91b04e
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-03-23 14:39:58 +0300

    added a ritl instruction

--- get_opcode_func.c
+++ get_opcode_func.c
@@ -21,6 +21,7 @@ void (*get_opcode_func(char *s))(stack_t **head, unsigned int line_num)
 		{"mod", mod},
 		{"pchar", pchar},
 		{"pstr", pstr},
+		{"rotl", rotl},
 		{NULL, NULL}
 	};
 
---
+++ rotl.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * rotl - rotates the stack to the top
+ * @stack: double pointer to the beginning of the stack
+ * @line_number: line number of the opcode being executed
+ */
+void rotl(stack_t **stack, unsigned int line_number)
+{
+    stack_t *last_node, *second_last_node;
+
+    (void) line_number; /* unused parameter */
+
+    if (*stack == NULL || (*stack)->next == NULL)
+        return;
+
+    last_node = *stack;
+    while (last_node->next != NULL)
+        last_node = last_node->next;
+    second_last_node = last_node->prev;
+
+    last_node->next = *stack;
+    (*stack)->prev = last_node;
+    *stack = last_node;
+    second_last_node->next = NULL;
+    last_node->prev = NULL;
+}
